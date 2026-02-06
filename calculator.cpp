eeb7c9bdd141d8e2632f42a76aeb2b0ff48e7bfc
Thandon-To <thanadonlungta@gmail.com>
2026-02-06 23:44:38 +0700

    change input to 999

--- calculator.cpp
+++ calculator.cpp
@@ -18,7 +18,7 @@ void DelNode(struct Node *now);
 // --- [ส่วนของคนที่ 1] Main ---
 int main() {
     struct Node *start = NULL;
-    char input[50];
+    char input[999];
 
     printf("--- Calculator (No ctype/stdlib) ---\n");
     printf("Input Expression: ");
